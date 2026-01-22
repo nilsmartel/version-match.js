31f2eb32430e3f30d63d563f9bc865b28de987c8
Nils Martel <nilsmartel@yahoo.de>
2026-01-22 17:18:06 +0100

    Write function to parse arguments

---
+++ main.ts
@@ -0,0 +1,24 @@
+function parseArgs() {
+    const argv = process.argv.slice(2)
+
+    let targetFlag = false
+    let target: string | undefined = undefined
+    const files: string[] = []
+
+
+    for (let a of argv) {
+        if (a == "--target") {
+            targetFlag = true
+            continue
+        }
+
+        if (targetFlag === true) {
+            target = a
+            continue
+        }
+
+        files.push(a)
+    }
+
+    return { files, target }
+}
