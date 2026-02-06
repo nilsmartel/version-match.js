45f8fb7dc1caa09741986bbd4899a87583b0c109
Nils Martel <nilsmartel@yahoo.de>
2026-02-06 10:54:36 +0100

    format code

--- main.ts
+++ main.ts
@@ -1,62 +1,68 @@
 import fs from "fs"
 
-
 function parseArgs() {
-    const argv = process.argv.slice(2)
+  const argv = process.argv.slice(2)
 
-    let targetFlag = false
-    let target: string | undefined = undefined
-    const files: string[] = []
+  let targetFlag = false
+  let target: string | undefined = undefined
+  const files: string[] = []
 
-
-    for (let a of argv) {
-        if (a == "--target") {
-            targetFlag = true
-            continue
-        }
-
-        if (targetFlag === true) {
-            target = a
-            continue
-        }
-
-        files.push(a)
-      targetFlag = false
+  for (let a of argv) {
+    if (a == "--target") {
+      targetFlag = true
+      continue
     }
 
-    if (!target) throw "expect file passed after --target flag. I.e. --target package.json"
+    if (targetFlag === true) {
+      target = a
+      targetFlag = false
+      continue
+    }
 
-    return { files, target }
+    files.push(a)
+  }
+
+  if (!target)
+    throw "expect file passed after --target flag. I.e. --target package.json"
+
+  return { files, target }
 }
-
 
 function getDependencies(files: string[]) {
-    console.log("(info)", `reading ${files.length} files`)
+  console.log("(info)", `reading ${files.length} files`)
 
-    let depss: Record<string, string>[] = files
-        .map(s => String(fs.readFileSync(s)))
-        .map(s => JSON.parse(s)).map(pkg => ({ ...pkg.dependencies, ...pkg.devDependencies } as Record<string, string>))
+  let depss: Record<string, string>[] = files
+    .map((s) => String(fs.readFileSync(s)))
+    .map((s) => JSON.parse(s))
+    .map(
+      (pkg) =>
+        ({ ...pkg.dependencies, ...pkg.devDependencies }) as Record<
+          string,
+          string
+        >,
+    )
 
-    let depMap: Record<string, string> = {}
+  let depMap: Record<string, string> = {}
 
-    for (let dep of depss) {
-        for (let [key, value] of Object.entries(dep)) {
-            let cur = depMap[key]
-            if (cur > value) continue
+  for (let dep of depss) {
+    for (let [key, value] of Object.entries(dep)) {
+      let cur = depMap[key]
+      if (cur > value) continue
 
-            depMap[key] = value
-        }
+      depMap[key] = value
     }
+  }
 
-    console.log("(info)", `found info on ${Object.keys(depMap).length} dependencies`)
+  console.log(
+    "(info)",
+    `found info on ${Object.keys(depMap).length} dependencies`,
+  )
 
-    return depMap
+  return depMap
 }
-
 
 let { files, target } = parseArgs()
 let deps = getDependencies(files)
-
 
 console.log("(info)", "reading " + target)
 let pkgJsonContent = String(fs.readFileSync(target!))
@@ -64,22 +70,20 @@ let pkgJson = JSON.parse(pkgJsonContent)
 
 // For all kinds of dependencies, look up of we have information about a newer version we can use instead
 for (let d of [pkgJson.dependencies, pkgJson.devDependencies]) {
-    for (let key in d) {
-        // version of dependency in keys
-        let dv = deps[key]
-        let v = d[key]
+  for (let key in d) {
+    // version of dependency in keys
+    let dv = deps[key]
+    let v = d[key]
 
-        if (dv > v) {
-            console.log("(info)", `${key}: ${v} => ${dv}`)
-            d[key] = dv
-        }
+    if (dv > v) {
+      console.log("(info)", `${key}: ${v} => ${dv}`)
+      d[key] = dv
     }
+  }
 }
-
 
 let output = JSON.stringify(pkgJson, null, 2)
 
 console.log("(info)", "writing " + target)
 
 fs.writeFileSync(target, output)
-
